20096272c0f9c35171fe00cab3d5c6b94ed67e09
Champetier <todave11@gmail.com>
2018-11-28 12:09:28 +0100

    menu + Lister les sessions

--- ihm.js
+++ ihm.js
@@ -1,7 +1,45 @@
 var service = require('./service');
+var readline = require('readline');
 
-exports.start = function() {
-    service.init(function(nb) {
-        console.log('[init]', nb, 'sessions trouvées.')
-    });
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+var textMenu = `************************* 
+1. Rafraichir les données 
+2. Lister les sessions
+99. Quitter\n`
+
+var promise1 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve('foo');
+    }, 300);
+});
+
+exports.start = function () {
+    promise1.then(rl.question(textMenu, function (saisie) {
+        service.init(function (nb) {
+            console.log('[init]', nb, 'sessions trouvées.')
+
+            switch (saisie) {
+                case '1':
+                    service.init(function (nb) {
+                        console.log('[init]', nb, 'sessions trouvées.')
+                    });
+                    break;
+                case '2':
+                    service.listerSessions(function (callback) {
+                        callback.forEach(tabTalk => {
+                            console.log('[talks]', tabTalk.name,"(",tabTalk.speakers,")");
+                        });    
+                    }
+                    )
+                    break;
+                default:
+                    break;
+            }
+            rl.close();
+        });
+    }))
 };
--- service.js
+++ service.js
@@ -1,29 +1,31 @@
 // tableau qui contiendra toutes les sessions du BreizhCamp
-talks = 0;
+//talks = 0;
+talks=[]
 var request = require('request')
 
 exports.init = function (callback) {
 
-    var varListJSON = ['http://2018.breizhcamp.org/json/talks.json', 'http://2018.breizhcamp.org/json/others.json']
+    request('http://2018.breizhcamp.org/json/talks.json', { json: true }, function (err, res, body) {
+        if (err) { return console.log('Erreur', err); }
 
-
-    for (let index = 0; index < varListJSON.length; index++) {
-
-
-        request(varListJSON[index], { json: true }, function (err, res, body) {
+        //une fois les données récupérées, alimenter la variable talks
+        //talks += body.length;
+        talks=talks.concat(body)
+        //invoquer la callback avec le nombre de sessions récupérées
+        request('http://2018.breizhcamp.org/json/others.json', { json: true }, function (err, res, body) {
             if (err) { return console.log('Erreur', err); }
+            // une fois les données récupérées, alimenter la variable talks
+            //talks += body.length;
+            talks=talks.concat(body)
 
-            // body contient les données récupérées
-            //console.log('Ok', body);
-            // TODO     => une fois les données récupérées, alimenter la variable talks
-            talks += body.length;
-            // TODO         => invoquer la callback avec le nombre de sessions récupérées
-            if(index==varListJSON.length-1){
-                callback(talks);
-            }
+            //invoquer la callback avec le nombre de sessions récupérées
+           //callback(talks);
+           callback(talks.length)
         });
+    });
 
-    }
-
-    
 };
+
+exports.listerSessions=function (callback) {
+    callback(talks)
+};    
